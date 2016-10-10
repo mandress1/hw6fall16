0092ee49d93ca71c616510c0c09527589b7a814f
Matt Andress <matthew-andress@uiowa.edu>
2016-10-10 19:55:14 +0000

    installed themoviedb gem and uncommented the class definition

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,20 +1,22 @@
+require 'themoviedb'
+
 class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+class Movie::InvalidKeyError < StandardError ; end
   
-# class Movie::InvalidKeyError < StandardError ; end
-  
-#  def self.find_in_tmdb(string)
-#    begin
-#      Tmdb::Movie.find(string)
-#    rescue NoMethodError => tmdb_gem_exception
-#      if Tmdb::Api.response['code'] == '401'
-#        raise Movie::InvalidKeyError, 'Invalid API key'
-#      else
-#        raise tmdb_gem_exception
-#      end
-#    end
-#  end
+  def self.find_in_tmdb(string)
+    begin
+      Tmdb::Movie.find(string)
+    rescue NoMethodError => tmdb_gem_exception
+      if Tmdb::Api.response['code'] == '401'
+        raise Movie::InvalidKeyError, 'Invalid API key'
+      else
+        raise tmdb_gem_exception
+      end
+    end
+  end
 
 end
