02c22ce35356d1f0b0832a13b2cc33c73d89aa7e
Matt Andress <matthew-andress@uiowa.edu>
2016-10-16 02:15:59 +0000

    took out the tmdb gem stuff out of the root application file and put it back into the movie model. not sure if it goes in the controller or the model though

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 require 'themoviedb'
 
 class Movie < ActiveRecord::Base
-  Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+  #Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
