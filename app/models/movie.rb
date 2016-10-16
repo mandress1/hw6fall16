1a26aada31de081e693f38b2561817ccf20bc16e
Matt Andress <matthew-andress@uiowa.edu>
2016-10-16 04:44:01 +0000

    shit added and no have a search box that doesn't do anything

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 require 'themoviedb'
 
 class Movie < ActiveRecord::Base
-  #Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+  
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
@@ -9,6 +9,7 @@ class Movie < ActiveRecord::Base
   class Movie::InvalidKeyError < StandardError ; end
   
   def self.find_in_tmdb(string)
+    Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562") 
     begin
       Tmdb::Movie.find(string)
     rescue NoMethodError => tmdb_gem_exception
